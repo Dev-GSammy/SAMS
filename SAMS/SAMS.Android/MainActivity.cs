2570e93bfd48bed12f63acf6cbbc56536239f592
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-07-28 06:14:06 +0100

    changed the reference to the launcher icon in android properties settings and the main activity

--- SAMS/SAMS.Android/MainActivity.cs
+++ SAMS/SAMS.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace SAMS.Droid
 {
-    [Activity(Label = "SAMS", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SAMS", Icon = "@mipmap/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
