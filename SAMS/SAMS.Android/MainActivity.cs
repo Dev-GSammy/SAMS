5e9451a4e9a7fad478391f8d3c851aec1d8576a7
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-07-28 07:18:31 +0100

    Now, this is what I'm talking about!!! I finally got the launcher icon to be exactly as I wanted. Perfect.

--- SAMS/SAMS.Android/MainActivity.cs
+++ SAMS/SAMS.Android/MainActivity.cs
@@ -5,9 +5,9 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
-namespace SAMS.Droid
+namespace SAMS.Droids
 {
-    [Activity(Label = "SAMS", Icon = "@mipmap/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SAMS", Icon = "@mipmap/app_icon_adaptive_fore", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
