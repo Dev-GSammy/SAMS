29b125cb482a2c12fe08056b515293e398980e1e
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-09-08 10:38:50 +0100

    Added the assembly and gave all fonts aliases

--- SAMS/SAMS/App.xaml.cs
+++ SAMS/SAMS/App.xaml.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+[assembly: ExportFont("Poppins-Black.ttf", Alias = "PoppinsBlack")]
+[assembly: ExportFont("Poppins-BlackItalic.ttf", Alias = "PoppinsBlackItalic")]
+[assembly: ExportFont("Poppins-Bold.ttf", Alias = "PoppinsBold")]
+[assembly: ExportFont("Poppins-BoldItalic.ttf", Alias = "PoppinsBoldItalic")]
+[assembly: ExportFont("Poppins-Medium.ttf", Alias = "PoppinsMedium")]
+[assembly: ExportFont("Poppins-MediumItalic.ttf", Alias = "PoppinsMediumItalic")]
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "PoppinsRegular")]
 
 namespace SAMS
 {
