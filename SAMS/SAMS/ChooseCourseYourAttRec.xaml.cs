5c008320d50944aa0478f6600abddd3e365c3f07
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-09-17 03:35:41 +0100

    Implemented the button from the choosecourseattendancerecord page. I hope it works.

--- SAMS/SAMS/ChooseCourseYourAttRec.xaml.cs
+++ SAMS/SAMS/ChooseCourseYourAttRec.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit.Extensions;
+using System.ComponentModel;
+using Xamarin.CommunityToolkit;
 
 namespace SAMS
 {
@@ -20,9 +23,9 @@ namespace SAMS
             PatternImage.Source = ImageSource.FromResource("SAMS.Assets.Images.WavyPattern.png", assembly);
         }
 
-        private void ChooseCourseYourAttRecbtn_Clicked(object sender, EventArgs e)
+        async private void ChooseCourseYourAttRecbtn_Clicked(object sender, EventArgs e)
         {
-
+            await App.Current.MainPage.Navigation.ShowPopupAsync(new AttendanceRecordPopup());
         }
     }
 }
