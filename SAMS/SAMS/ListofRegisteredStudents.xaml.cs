8f357d40cad9479c88f4cb98d88ba2e4b6ede655
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-09-25 20:29:09 +0100

    Added a confirmation display alert to the give permission page and button

--- SAMS/SAMS/ListofRegisteredStudents.xaml.cs
+++ SAMS/SAMS/ListofRegisteredStudents.xaml.cs
@@ -19,7 +19,8 @@ namespace SAMS
 
         private void GivePermissionbtn_Clicked(object sender, EventArgs e)
         {
-
+            DisplayAlert("Give Permission", "Are you sure you want to give Samuel Permission to mark attendance for the ECN310 course?", "Yes", "No");
+            
         }
     }
 }
