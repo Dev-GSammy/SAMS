9ede19f63f0a95767b4b2a5a8609a27750a09cc9
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-09-29 11:00:03 +0100

    Implementation of the Popup page is quite not very familiar yet.

--- SAMS/SAMS/DashboardMisc.xaml.cs
+++ SAMS/SAMS/DashboardMisc.xaml.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit.Extensions;
+using System.ComponentModel;
+using Xamarin.CommunityToolkit;
 
 namespace SAMS
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class DashboardMisc : ContentPage
+    public partial class DashboardMisc
     {
         public DashboardMisc()
         {
