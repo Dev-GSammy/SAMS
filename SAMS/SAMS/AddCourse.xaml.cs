f73ebc48cf33396f44495babb5ba2c09964d3025
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2023-05-17 15:37:03 +0100

    Created dummy details to fill the listboxes and grid views in all affected files. Changed some details in the xaml document of these files.

--- SAMS/SAMS/AddCourse.xaml.cs
+++ SAMS/SAMS/AddCourse.xaml.cs
@@ -17,6 +17,7 @@ namespace SAMS
             var assembly = typeof(ForgotPassword);
 
             PatternImage.Source = ImageSource.FromResource("SAMS.Assets.Images.WavyPattern.png", assembly);
+            Addcourselv.ItemsSource = new string[] { "CSC 101 - Introduction to Computer Science", "IFT 202 - Introduction to Information Technology", "PMT 203 - Principles of Management", "ENT 204 - Entrepreneurship", "SEN 205 - Sensory Perception", "CYS 206 - Cell and Molecular Biology", "CSC 301 - Data Structures and Algorithms", "IFT 302 - Database Systems", "PMT 303 - Organizational Behavior", "ENT 304 - Financial Accounting", "SEN 305 - Human Anatomy and Physiology", "CYS 306 - Genetics", "CSC 401 - Computer Architecture", "IFT 402 - Software Engineering", "PMT 403 - Marketing", "ENT 404 - Managerial Accounting", "SEN 405 - Microbiology", "CYS 406 - Immunology" };
         }
 
         private void AddCoursebtn_Clicked(object sender, EventArgs e)
--- SAMS/SAMS/ChooseStudentFile.xaml.cs
+++ SAMS/SAMS/ChooseStudentFile.xaml.cs
@@ -19,7 +19,8 @@ namespace SAMS
 
             PatternImage.Source = ImageSource.FromResource("SAMS.Assets.Images.WavyPattern.png", assembly);
             //UploadStudentCSVbtn.ImageSource = ImageSource.FromResource("SAMS.Assets.Icons.LockIcon.png", assembly);
-            ChooseStudentFilelv.ItemsSource = new string[] { "500L IFT", "200L CSC", "300L SEN", "500L Special Students", "300L Carry Over Students" };
+            
+            ChooseStudentFilelv.ItemsSource = new string[] {"Computer Science Department 100L List", "Computer Science Department 200L List", "Computer Science Department 300L List", "Computer Science Department 400L List", "Information Technology Department 100L List", "Information Technology Department 200L List", "Information Technology Department 300L List", "Information Technology Department 400L List", "Management Department 100L List", "Management Department 200L List", "Management Department 300L List", "Management Department 400L List", "Entrepreneurship Department 100L List", "Entrepreneurship Department 200L List", "Entrepreneurship Department 300L List", "Entrepreneurship Department 400L List", "Sensory Perception Department 100L List", "Sensory Perception Department 200L List", "Sensory Perception Department 300L List", "Sensory Perception Department 400L List", "Cell and Molecular Biology Department 100L List", "Cell and Molecular Biology Department 200L List", "Cell and Molecular Biology Department 300L List", "Cell and Molecular Biology Department 400L List"};
         }
 
         private void UploadStudentCSVbtn_Clicked(object sender, EventArgs e)
